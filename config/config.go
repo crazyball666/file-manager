9936809410000942af5fb4862546b44b195f8e1b
sujiahao <774028406@qq.com>
2020-08-18 23:05:26 +0800

    修改样式

--- config/config.go
+++ config/config.go
@@ -1,9 +1,9 @@
 package config
 
 var (
-	ServerPort int = 8080 //端口号
-	//RootPath string = "/Users/crazyball/Desktop"
-	RootPath      string = "/root/crazyball/static" // 根路径
+	ServerPort    int    = 8080 //端口号
+	//RootPath      string = "/Users/crazyball"
+	RootPath      string = "/root/crazyball"        // 根路径
 	UploadDirName string = "uploads"                // 上传文件夹
 	StaticPath    string = "/root/crazyball/static" // 静态资源地址
 	StaticHost    string = "static.crazyball.xyz"
